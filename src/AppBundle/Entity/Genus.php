bd67afeebc7c9b02779fdae6a84b6fce05675d67
Nathan Hymers <nathan.hymers@dgkc2hgcjv3x.wren.wrenkitchens.com>
2020-02-21 16:29:44 +0000

    First commit

---
+++ src/AppBundle/Entity/Genus.php
@@ -0,0 +1,113 @@
+<?php
+
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+// These annotations mark this class as an Entity class so that doctrine will use this class to map a database table and its columns
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="genus")
+ */
+class Genus
+{
+    // The annotations above the properties are what tell doctrine that these are columns, it will take the names of the properties and those will be the names of the columns in the database table genus
+    // The ID will always look the same since it is the primary key.
+    // The Column type annotation is what type the data will be.
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $subFamily;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $speciesCount;
+
+    // If you want a column in the database to be optional and not required to have data for every entry, use "nullable=true"
+    /**
+     * @ORM\Column(type="string", nullable=true)
+     */
+    private $funFact;
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSubFamily()
+    {
+        return $this->subFamily;
+    }
+
+    /**
+     * @param mixed $subFamily
+     */
+    public function setSubFamily($subFamily)
+    {
+        $this->subFamily = $subFamily;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSpeciesCount()
+    {
+        return $this->speciesCount;
+    }
+
+    /**
+     * @param mixed $speciesCount
+     */
+    public function setSpeciesCount($speciesCount)
+    {
+        $this->speciesCount = $speciesCount;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFunFact()
+    {
+        return $this->funFact;
+    }
+
+    /**
+     * @param mixed $funFact
+     */
+    public function setFunFact($funFact)
+    {
+        $this->funFact = $funFact;
+    }
+
+
+
+
+}
